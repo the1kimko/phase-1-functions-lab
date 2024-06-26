12800e298082d7da01bf2e5c614139a82d9938dc
KomfortKimko <komfortkimk@gmail.com>
2024-06-26 12:48:52 +0300

    Functions lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const hq = 42;
+
+    return Math.abs(location - hq);
+}
+
+function distanceFromHqInFeet(location) {
+    const blockFeet = 264;
+    
+    return distanceFromHqInBlocks(location) * blockFeet;
+}
+
+function distanceTravelledInFeet(start, finish) {
+    const passengerTravel = Math.abs(start - finish);
+    const blockFeet = 264;
+
+    return passengerTravel * blockFeet;
+}
+
+function calculatesFarePrice(start, finish) {
+   const distance = distanceTravelledInFeet(start, finish);
+   
+   if (distance < 400) {
+    return 0;
+   } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+   } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+   } else {
+    return 'cannot travel that far';
+   }
+}
